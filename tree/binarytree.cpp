29a58208e1b1251b1ce34a3f8e7c7326736b0bdb
Bishal Paul <bishalpaul1905@gmail.com>
2017-10-27 00:55:43 +0530

    Basic Binary Tree functionality defined

--- tree/binarytree.cpp
+++ tree/binarytree.cpp
@@ -1,8 +1,47 @@
 #include<iostream>
 using namespace std;
 
+// Binary Tree defination
+struct node{
+  int data;
+  struct node * right;
+  struct node * left;
+
+};
+// Function for creating a new node with a intezer
+struct node * newnode(int element){
+  struct node * new_node;
+  new_node = (* node)malloc(sizeof(node));
+  new_node->data = element;
+  new_node->right = NULL;
+  new_node->left = NULL;
+  return new_node;  
+}
+// Function for calculating maximun height/depth of tree
+int maxDepth( struct node * NODE ){
+  if ( NODE == NULL )
+    return 0;
+  else{
+    int ldepth = maxDepth( NODE->left );
+    int rdepth = maxDepth( NODE->eight );
+    if( ldepth > rdepth )
+      return (ldepth + 1);
+    else
+      return (rdepth + 1);
+  }
+}
+
+// Main function
 int main(){
 
+  // Creating a node
+  struct node root;
+  //Pointing to a node
+  struct  node * root;
+  root = (node *)malloc(sizeof(node));
 
+
+
+  
   return 0;
 }
